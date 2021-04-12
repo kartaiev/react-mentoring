75fc4dfa228a9bbb514d2bf6abe6ccf5bc5d9ba6
Volodymyr Kartaiev <volodymyr.kartaiev@refinitiv.com>
2021-04-12 08:29:06 +0300

    fourth Task

--- src/state/actions/moviesActions.ts
+++ src/state/actions/moviesActions.ts
@@ -13,164 +13,141 @@ import axios, { AxiosRequestConfig } from 'axios';
 import { Dispatch } from 'redux';
 import { MoviesAction, MovieType } from '../interfaces';
 
+const {
+  GET_MOVIES,
+  GET_MOVIES_SUCCESS,
+  GET_MOVIES_ERROR,
+  SEARCH_MOVIES,
+  SEARCH_MOVIES_SUCCESS,
+  SEARCH_MOVIES_ERROR,
+  DELETE_MOVIE,
+  DELETE_MOVIE_SUCCESS,
+  DELETE_MOVIE_ERROR,
+  UPDATE_POSTER,
+  UPDATE_POSTER_SUCCESS,
+  UPDATE_POSTER_ERROR,
+  UPDATE_MOVIE,
+  UPDATE_MOVIE_SUCCESS,
+  UPDATE_MOVIE_ERROR,
+  ADD_MOVIE,
+  ADD_MOVIE_SUCCESS,
+  ADD_MOVIE_ERROR,
+  FILTER_MOVIES,
+  FILTER_MOVIES_SUCCESS,
+  FILTER_MOVIES_ERROR,
+  SORT_MOVIES,
+  SORT_MOVIES_SUCCESS,
+  SORT_MOVIES_ERROR
+} = MoviesActionType;
+
+const pending = (type: MoviesActionType) => ({
+  type
+});
+
+const success = (type: MoviesActionType, payload: MovieType | number) => ({
+  type,
+  payload
+});
+
+const error = (type: MoviesActionType, payload: string) => ({
+  type,
+  payload
+});
+
 export const getMovies = (offset = 0, limit = 12) => {
   return async (dispatch: Dispatch<MoviesAction>): Promise<void> => {
-    dispatch({
-      type: MoviesActionType.GET_MOVIES
-    });
+    dispatch(<MoviesAction>pending(GET_MOVIES));
     try {
       const { data } = await axios.request(getAllMovies(offset, limit));
-      dispatch({
-        type: MoviesActionType.GET_MOVIES_SUCCESS,
-        payload: data
-      });
+      dispatch(<MoviesAction>success(GET_MOVIES_SUCCESS, data));
     } catch (e) {
-      dispatch({
-        type: MoviesActionType.GET_MOVIES_ERROR,
-        payload: e.message
-      });
+      dispatch(<MoviesAction>error(GET_MOVIES_ERROR, e.message));
     }
   };
 };
 
 export const searchMovies = (title: string) => {
   return async (dispatch: Dispatch<MoviesAction>): Promise<void> => {
-    dispatch({
-      type: MoviesActionType.SEARCH_MOVIES
-    });
+    dispatch(<MoviesAction>pending(SEARCH_MOVIES));
     try {
       const { data } = await axios.request(searchMoviesByTitle(title));
-      dispatch({
-        type: MoviesActionType.SEARCH_MOVIES_SUCCESS,
-        payload: data
-      });
+      dispatch(<MoviesAction>success(SEARCH_MOVIES_SUCCESS, data));
     } catch (e) {
-      dispatch({
-        type: MoviesActionType.SEARCH_MOVIES_ERROR,
-        payload: e.message
-      });
+      dispatch(<MoviesAction>error(SEARCH_MOVIES_ERROR, e.message));
     }
   };
 };
 
 export const deleteMovie = (id: number) => {
   return async (dispatch: Dispatch<MoviesAction>): Promise<void> => {
-    dispatch({
-      type: MoviesActionType.DELETE_MOVIE
-    });
+    dispatch(<MoviesAction>pending(DELETE_MOVIE));
     try {
       await axios.request(deleteSingleMovie(id));
-      dispatch({
-        type: MoviesActionType.DELETE_MOVIE_SUCCESS,
-        payload: id
-      });
+      dispatch(<MoviesAction>success(DELETE_MOVIE_SUCCESS, id));
     } catch (e) {
-      dispatch({
-        type: MoviesActionType.DELETE_MOVIE_ERROR,
-        payload: e.message
-      });
+      dispatch(<MoviesAction>error(DELETE_MOVIE_ERROR, e.message));
     }
   };
 };
 
 export const updatePoster = (movie: MovieType) => {
   return async (dispatch: Dispatch<MoviesAction>): Promise<void> => {
-    dispatch({
-      type: MoviesActionType.UPDATE_POSTER
-    });
+    dispatch(<MoviesAction>pending(UPDATE_POSTER));
     try {
       const { data } = await axios.request(getPoster(movie.title));
       movie.poster_path = data.titles[0].image;
       await axios.request(<AxiosRequestConfig>updateSingleMovie(movie));
-      dispatch({
-        type: MoviesActionType.UPDATE_POSTER_SUCCESS,
-        payload: movie
-      });
+      dispatch(<MoviesAction>success(UPDATE_POSTER_SUCCESS, movie));
     } catch (e) {
-      dispatch({
-        type: MoviesActionType.UPDATE_POSTER_ERROR,
-        payload: e.message
-      });
+      dispatch(<MoviesAction>error(UPDATE_POSTER_ERROR, e.message));
     }
   };
 };
 
 export const updateMovie = (movie: MovieType) => {
   return async (dispatch: Dispatch<MoviesAction>): Promise<void> => {
-    dispatch({
-      type: MoviesActionType.UPDATE_MOVIE
-    });
+    dispatch(<MoviesAction>pending(UPDATE_MOVIE));
     try {
       await axios.request(<AxiosRequestConfig>updateSingleMovie(movie));
-      dispatch({
-        type: MoviesActionType.UPDATE_MOVIE_SUCCESS,
-        payload: movie
-      });
+      dispatch(<MoviesAction>success(UPDATE_MOVIE_SUCCESS, movie));
     } catch (e) {
-      dispatch({
-        type: MoviesActionType.UPDATE_MOVIE_ERROR,
-        payload: e.message
-      });
+      dispatch(<MoviesAction>error(UPDATE_MOVIE_ERROR, e.message));
     }
   };
 };
 
 export const addMovie = (movie: MovieType) => {
   return async (dispatch: Dispatch<MoviesAction>): Promise<void> => {
-    dispatch({
-      type: MoviesActionType.ADD_MOVIE
-    });
+    dispatch(dispatch(<MoviesAction>pending(ADD_MOVIE)));
     try {
       await axios.request(<AxiosRequestConfig>addSingleMovie(movie));
-      dispatch({
-        type: MoviesActionType.ADD_MOVIE_SUCCESS,
-        payload: movie
-      });
+      dispatch(<MoviesAction>success(ADD_MOVIE_SUCCESS, movie));
     } catch (e) {
-      dispatch({
-        type: MoviesActionType.ADD_MOVIE_ERROR,
-        payload: e.message
-      });
+      dispatch(<MoviesAction>error(ADD_MOVIE_ERROR, e.message));
     }
   };
 };
 
 export const filterMovies = (genre: string) => {
   return async (dispatch: Dispatch<MoviesAction>): Promise<void> => {
-    dispatch({
-      type: MoviesActionType.FILTER_MOVIES
-    });
+    dispatch(<MoviesAction>pending(FILTER_MOVIES));
     try {
       const { data } = await axios.request(filterAllMovies(genre));
-      dispatch({
-        type: MoviesActionType.FILTER_MOVIES_SUCCESS,
-        payload: data
-      });
+      dispatch(<MoviesAction>success(FILTER_MOVIES_SUCCESS, data));
     } catch (e) {
-      dispatch({
-        type: MoviesActionType.FILTER_MOVIES_ERROR,
-        payload: e.message
-      });
+      dispatch(<MoviesAction>error(FILTER_MOVIES_ERROR, e.message));
     }
   };
 };
 
 export const sortMovies = (sortBy: string) => {
   return async (dispatch: Dispatch<MoviesAction>): Promise<void> => {
-    dispatch({
-      type: MoviesActionType.SORT_MOVIES
-    });
+    dispatch(<MoviesAction>pending(SORT_MOVIES));
     try {
       const { data } = await axios.request(sortAllMovies(sortBy));
-      dispatch({
-        type: MoviesActionType.SORT_MOVIES_SUCCESS,
-        payload: data
-      });
+      dispatch(<MoviesAction>success(SORT_MOVIES_SUCCESS, data));
     } catch (e) {
-      dispatch({
-        type: MoviesActionType.SORT_MOVIES_ERROR,
-        payload: e.message
-      });
+      dispatch(<MoviesAction>error(SORT_MOVIES_ERROR, e.message));
     }
   };
 };
--- src/utils/axiosOptions.ts
+++ src/utils/axiosOptions.ts
@@ -1,99 +1,68 @@
 import { AxiosRequestConfig } from 'axios';
 import { MovieType } from '../state/interfaces';
 
-export const getOptions = (
-  param1: string,
-  param2: string
-): AxiosRequestConfig => {
-  return {
-    method: 'GET',
-    url: `http://www.omdbapi.com/?${param1}=${param2}&apikey=484ceffb`
-  };
-};
-
 export const getAllMovies = (
   offset: number,
   limit: number
-): AxiosRequestConfig => {
-  return {
-    method: 'GET',
-    url: `http://localhost:4000/movies?offset=${offset}&limit=${limit}`
-  };
-};
+): AxiosRequestConfig => ({
+  method: 'GET',
+  url: `http://localhost:4000/movies?offset=${offset}&limit=${limit}`
+});
 
-export const filterAllMovies = (genre: string): AxiosRequestConfig => {
-  return {
-    method: 'GET',
-    url: `http://localhost:4000/movies?sortBy=title&filter=${genre}&offset=1&limit=12`
-  };
-};
+export const filterAllMovies = (genre: string): AxiosRequestConfig => ({
+  method: 'GET',
+  url: `http://localhost:4000/movies?sortBy=title&filter=${genre}&offset=1&limit=12`
+});
 
-export const getSingleMovie = (id: number): AxiosRequestConfig => {
-  return {
-    method: 'GET',
-    url: `http://localhost:4000/movies/${id}`
-  };
-};
+export const getSingleMovie = (id: number): AxiosRequestConfig => ({
+  method: 'GET',
+  url: `http://localhost:4000/movies/${id}`
+});
 
-export const searchMoviesByTitle = (title: string): AxiosRequestConfig => {
-  return {
-    method: 'GET',
-    url: `http://localhost:4000/movies?search=${title}&searchBy=title`
-  };
-};
+export const searchMoviesByTitle = (title: string): AxiosRequestConfig => ({
+  method: 'GET',
+  url: `http://localhost:4000/movies?search=${title}&searchBy=title&offset=1&limit=12`
+});
 
-export const sortAllMovies = (sortBy: string): AxiosRequestConfig => {
-  return {
-    method: 'GET',
-    url: `http://localhost:4000/movies?sortBy=${sortBy}&sortOrder=desc&offset=1&limit=12`
-  };
-};
+export const sortAllMovies = (sortBy: string): AxiosRequestConfig => ({
+  method: 'GET',
+  url: `http://localhost:4000/movies?sortBy=${sortBy}&sortOrder=desc&offset=1&limit=12`
+});
 
-export const deleteSingleMovie = (id: number): AxiosRequestConfig => {
-  return {
-    method: 'DELETE',
-    url: `http://localhost:4000/movies/${id}`
-  };
-};
+export const deleteSingleMovie = (id: number): AxiosRequestConfig => ({
+  method: 'DELETE',
+  url: `http://localhost:4000/movies/${id}`
+});
 
-export const getPoster = (title: string): AxiosRequestConfig => {
-  return {
-    method: 'GET',
-    url: `https://imdb-internet-movie-database-unofficial.p.rapidapi.com/search/${title}`,
-    headers: {
-      'x-rapidapi-key': '25b376b5b0msh398195bc57cbd33p1ace86jsn9aaf1e023d34',
-      'x-rapidapi-host':
-        'imdb-internet-movie-database-unofficial.p.rapidapi.com'
-    }
-  };
-};
+export const getPoster = (title: string): AxiosRequestConfig => ({
+  method: 'GET',
+  url: `https://imdb-internet-movie-database-unofficial.p.rapidapi.com/search/${title}`,
+  headers: {
+    'x-rapidapi-key': '25b376b5b0msh398195bc57cbd33p1ace86jsn9aaf1e023d34',
+    'x-rapidapi-host': 'imdb-internet-movie-database-unofficial.p.rapidapi.com'
+  }
+});
 
-export const updateMoviePoster = (movie: MovieType): AxiosRequestConfig => {
-  return {
-    method: 'PUT',
-    data: JSON.stringify(movie),
-    url: `http://localhost:4000/movies/`
-  };
-};
+export const updateMoviePoster = (movie: MovieType): AxiosRequestConfig => ({
+  method: 'PUT',
+  data: JSON.stringify(movie),
+  url: `http://localhost:4000/movies/`
+});
 
-export const updateSingleMovie = (movie: MovieType): AxiosRequestConfig => {
-  return {
-    method: 'PUT',
-    data: JSON.stringify(movie),
-    url: `http://localhost:4000/movies/`,
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  };
-};
+export const updateSingleMovie = (movie: MovieType): AxiosRequestConfig => ({
+  method: 'PUT',
+  data: JSON.stringify(movie),
+  url: `http://localhost:4000/movies/`,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
 
-export const addSingleMovie = (movie: MovieType): AxiosRequestConfig => {
-  return {
-    method: 'POST',
-    data: JSON.stringify(movie),
-    url: `http://localhost:4000/movies/`,
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  };
-};
+export const addSingleMovie = (movie: MovieType): AxiosRequestConfig => ({
+  method: 'POST',
+  data: JSON.stringify(movie),
+  url: `http://localhost:4000/movies/`,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
